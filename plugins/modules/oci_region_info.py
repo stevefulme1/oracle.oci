4d1c0eee32566f1e7a5d5fb4202ccfff29c978c6
sfulmer <sfulmer@redhat.com>
2026-05-16 21:44:29 -0400

    feat(oci): implement oci_region_info - complete stub module

--- plugins/modules/oci_region_info.py
+++ plugins/modules/oci_region_info.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2024, Oracle and/or its affiliates.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-"""Ansible module for retrieving OCI region information."""
+"""Ansible module for retrieving OCI regions information."""
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -18,22 +18,22 @@ version_added: "2.2.0"
 author:
     - Oracle (@oracle)
 options:
-    compartment_id:
-        description:
-            - The OCID of the compartment.
-        type: str
+extends_documentation_fragment:
+    - stevefulme1.oci_cloud.oci_common
+requirements:
+    - "python >= 3.8"
+    - "oci >= 2.90.0"
 """
 
 EXAMPLES = r"""
 - name: List regions
   stevefulme1.oci_cloud.oci_region_info:
-    compartment_id: "ocid1.compartment.oc1..example"
   register: result
 """
 
 RETURN = r"""
 regions:
-    description: List of region details.
+    description: List of regions details.
     returned: always
     type: list
     elements: dict
@@ -41,18 +41,42 @@ regions:
 
 from ansible.module_utils.basic import AnsibleModule
 
+try:
+    from oci.identity import IdentityClient
+    from oci.exceptions import ServiceError
+    HAS_OCI_SDK = True
+except ImportError:
+    HAS_OCI_SDK = False
+
+from ansible_collections.stevefulme1.oci_cloud.plugins.module_utils.oci_common import (
+    OCI_COMMON_ARGS,
+    to_dict,
+)
+from ansible_collections.stevefulme1.oci_cloud.plugins.module_utils.oci_auth import create_service_client
+from ansible_collections.stevefulme1.oci_cloud.plugins.module_utils.oci_wait import call_with_retry
+
 
 def main():
     module_args = dict(
-        compartment_id=dict(type="str"),
     )
+    module_args.update(OCI_COMMON_ARGS)
 
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True,
     )
 
-    module.fail_json(msg="oci_region_info module is a stub. Full implementation requires OCI SDK integration.")
+    if not HAS_OCI_SDK:
+        module.fail_json(msg="The 'oci' Python SDK is required. Install with: pip install oci")
+
+    client = create_service_client(module, IdentityClient)
+
+    try:
+        response = call_with_retry(client.list_regions)
+        results = [to_dict(item) for item in response.data]
+        module.exit_json(changed=False, regions=results)
+    except ServiceError as e:
+        module.fail_json(msg=f"Failed to list regions: {e.message}")
 
 
 if __name__ == "__main__":
